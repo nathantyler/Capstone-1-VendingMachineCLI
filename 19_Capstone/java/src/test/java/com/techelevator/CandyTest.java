60b870f393d8451e82159cbc47363b886162cf15
Guan He <heguanelvis@gmail.com>
2019-10-10 14:43:14 -0400

    added basic tests

--- 19_Capstone/java/src/test/java/com/techelevator/CandyTest.java
+++ 19_Capstone/java/src/test/java/com/techelevator/CandyTest.java
@@ -2,15 +2,19 @@ package com.techelevator;
 
 import static org.junit.Assert.*;
 
+import java.math.BigDecimal;
+
 import org.junit.Test;
 
 public class CandyTest {
 
-	Item candyTest = new Candy();
+	Item candyTest = new Candy("Sour Patch", BigDecimal.valueOf(3.5));
 	
 	@Test
-	public void test() {
-		assertEquals("sourpatch", candyTest.getName());
+	public void gettersShouldReturnRelevantValues() {
+		assertEquals("Sour Patch", candyTest.getName());
+		assertEquals(BigDecimal.valueOf(3.5), candyTest.getPrice());
+		assertEquals("Candy", candyTest.getType());
 	}
 
 }
