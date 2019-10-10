5299b549f953a44268ae17f6359e367586250384
Guan He <heguanelvis@gmail.com>
2019-10-10 14:54:47 -0400

    added setter test

--- 19_Capstone/java/src/test/java/com/techelevator/CandyTest.java
+++ 19_Capstone/java/src/test/java/com/techelevator/CandyTest.java
@@ -15,6 +15,19 @@ public class CandyTest {
 		assertEquals("Sour Patch", candyTest.getName());
 		assertEquals(BigDecimal.valueOf(3.5), candyTest.getPrice());
 		assertEquals("Candy", candyTest.getType());
+		assertEquals(5, candyTest.getStock());
 	}
 
+	@Test
+	public void settersShouldReturnRelevantValues() {
+		candyTest.setName("New Sour Patch");
+		candyTest.setStock(3);
+		candyTest.setPrice(BigDecimal.valueOf(4.0));
+		candyTest.setPosition("A1");
+		
+		assertEquals("New Sour Patch", candyTest.getName());
+		assertEquals(3, candyTest.getStock());
+		assertEquals(BigDecimal.valueOf(4.0), candyTest.getPrice());
+		assertEquals("A1", candyTest.getPosition());
+	}
 }
