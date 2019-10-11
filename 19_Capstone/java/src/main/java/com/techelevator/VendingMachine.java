cac7a84f1db7c9def9668108467ce780bbecded8
Nathan Newcomb <stanlarson@gmail.com>
2019-10-11 08:30:58 -0400

    Added itemSelector map to VendingMachine.

--- 19_Capstone/java/src/main/java/com/techelevator/VendingMachine.java
+++ 19_Capstone/java/src/main/java/com/techelevator/VendingMachine.java
@@ -1,106 +1,106 @@
 package com.techelevator;
-
+ 
 import java.io.File;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-
+ 
 public class VendingMachine {
-
-	private List<Item> items;
-	private List<Chips> chips;
-	private List<Candy> candies;
-	private List<Drink> drinks;
-	private List<Gum> gums;
-
-	/**
-	 * @return the items
-	 */
-	public List<Item> getItems() {
-		return items;
-	}
-
-	/**
-	 * @return the chips
-	 */
-	public List<Chips> getChips() {
-		return chips;
-	}
-
-	/**
-	 * @return the candies
-	 */
-	public List<Candy> getCandies() {
-		return candies;
-	}
-
-	/**
-	 * @return the drinks
-	 */
-	public List<Drink> getDrinks() {
-		return drinks;
-	}
-
-	/**
-	 * @return the gums
-	 */
-	public List<Gum> getGums() {
-		return gums;
-	}
-
-	/**
-	 * 
-	 */
-	public VendingMachine() {
-		items = new ArrayList<Item>();
-		chips = new ArrayList<Chips>();
-		candies = new ArrayList<Candy>();
-		drinks = new ArrayList<Drink>();
-		gums = new ArrayList<Gum>();
-		try {
-			populateItems();
-		} catch (Exception e) {
-			System.out.println("Problem! vendingmachine.csv most likely not present.");
-		}
-
-	}
-
-	private void populateItems() throws IOException {
-		File file = new File("vendingmachine.csv");
-		try (Scanner reader = new Scanner(file.getAbsoluteFile())) {
-			while (reader.hasNextLine()) {
-				String line = reader.nextLine();
-				String[] itemsStrings = line.split("\\|");
-				//Item item = new Item(itemsStrings[0], itemsStrings[1], new BigDecimal(itemsStrings[2]));
-				//items.add(item);
-				if (itemsStrings[3].equals("Chip")) {
-					Chips chip = new Chips(itemsStrings[0], itemsStrings[1], new BigDecimal(itemsStrings[2]));
-					chips.add(chip);
-					items.add(chip);
-				}
-				if (itemsStrings[3].equals("Candy")) {
-					Candy candy = new Candy(itemsStrings[0], itemsStrings[1], new BigDecimal(itemsStrings[2]));
-					candies.add(candy);
-					items.add(candy);
-				}
-				if (itemsStrings[3].equals("Drink")) {
-					Drink drink = new Drink(itemsStrings[0], itemsStrings[1], new BigDecimal(itemsStrings[2]));
-					drinks.add(drink);
-					items.add(drink);
-				}
-				if (itemsStrings[3].equals("Gum")) {
-					Gum gum = new Gum(itemsStrings[0], itemsStrings[1], new BigDecimal(itemsStrings[2]));
-					gums.add(gum);
-					items.add(gum);
-				}
-
-			}
-		} catch (Exception e) {
-			System.out.println("Problem encountered. The file vendingmachine.csv is probably missing. Exiting.");
-			System.exit(1);
-		}
-	}
-
+ 
+    private List<Item> items;
+    private List<Chips> chips;
+    private List<Candy> candies;
+    private List<Drink> drinks;
+    private List<Gum> gums;
+    private Map<String, Item> itemSelector; // If you think of a better name for this map feel free to change it.
+    private BigDecimal userBalance; // Guan, I moved this initialization to the constructor.
+ 
+    public VendingMachine() {
+        items = new ArrayList<Item>();
+        chips = new ArrayList<Chips>();
+        candies = new ArrayList<Candy>();
+        drinks = new ArrayList<Drink>();
+        gums = new ArrayList<Gum>();
+        itemSelector = new HashMap<String, Item>();
+        userBalance = new BigDecimal("0.00"); // When you initialize a BigDecimal like this it seems to automatically
+                                                // set the scale so it keeps it in that nice 0.00 money format.
+        try {
+            populateItems();
+        } catch (Exception e) {
+            System.out.println("Problem! vendingmachine.csv most likely not present.");
+        }
+    }
+ 
+    public List<Item> getItems() {
+        return items;
+    }
+ 
+    public BigDecimal getUserBalance() {
+        return userBalance;
+    }
+ 
+    public List<Chips> getChips() {
+        return chips;
+    }
+ 
+    public List<Candy> getCandies() {
+        return candies;
+    }
+ 
+    public List<Drink> getDrinks() {
+        return drinks;
+    }
+ 
+    public List<Gum> getGums() {
+        return gums;
+    }
+ 
+    public Map<String, Item> getItemSelector() {
+        return itemSelector;
+    }
+ 
+    public void addMoneyToMachine(BigDecimal money) {
+        userBalance = userBalance.add(money);
+    }
+ 
+    private void populateItems() throws IOException {
+        File file = new File("vendingmachine.csv");
+        try (Scanner reader = new Scanner(file.getAbsoluteFile())) {
+ 
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                String[] itemsStrings = line.split("\\|");
+                if (itemsStrings[3].equals("Chip")) {
+                    Chips chip = new Chips(itemsStrings[0], itemsStrings[1], new BigDecimal(itemsStrings[2]));
+                    chips.add(chip);
+                    items.add(chip);
+                    itemSelector.put(chip.getPosition(), chip);
+                } else if (itemsStrings[3].equals("Candy")) {
+                    Candy candy = new Candy(itemsStrings[0], itemsStrings[1], new BigDecimal(itemsStrings[2]));
+                    candies.add(candy);
+                    items.add(candy);
+                    itemSelector.put(candy.getPosition(), candy);
+                } else if (itemsStrings[3].equals("Drink")) {
+                    Drink drink = new Drink(itemsStrings[0], itemsStrings[1], new BigDecimal(itemsStrings[2]));
+                    drinks.add(drink);
+                    items.add(drink);
+                    itemSelector.put(drink.getPosition(), drink);
+                } else if (itemsStrings[3].equals("Gum")) {
+                    Gum gum = new Gum(itemsStrings[0], itemsStrings[1], new BigDecimal(itemsStrings[2]));
+                    gums.add(gum);
+                    items.add(gum);
+                    itemSelector.put(gum.getPosition(), gum);
+                }
+            }
+ 
+        } catch (Exception e) {
+            System.out.println("Problem encountered. The file vendingmachine.csv is probably missing. Exiting.");
+            System.exit(1);
+        }
+    }
+ 
 }
